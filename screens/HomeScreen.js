e62a2f33c5c7372d4ae7be76357e2efa57cd8003
Tomás <noronha.t@sapo.pt>
2023-02-06 18:07:58 +0000

    AA

---
+++ screens/HomeScreen.js
@@ -0,0 +1,54 @@
+import { View, Text, SafeAreaView, Image, TextInput, ScrollView } from "react-native";
+import React, { useLayoutEffect } from "react";
+import {ChevronDownIcon,UserIcon, AdjustmentsHorizontalIcon,MagnifyingGlassIcon} from "react-native-heroicons/outline";
+import Categories from "../components/Categories";
+
+const HomeScreen = () => {
+  // const navigation = useNavigation();
+
+  // useLayoutEffect(() => {
+  //   navigation.setOptions({
+  //     headerShown: false,
+  //   });
+  // }, []);
+
+  return (
+    
+    <SafeAreaView className="bg-white pt-10 ">
+      <View className="flex-row pb-3 items-center mx-4 space-x-2 ">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
+      <View className="flex-row items-center space-x-2 pb-2 mx-4 ">
+        <View className="flex-row space-x-2 bg-gray-200 p-3 flex-1">
+           <MagnifyingGlassIcon color="gray" /> 
+          <TextInput placeholder="Restaurants and cuisines" keyboardType="default" /> 
+        </View>
+         <AdjustmentsHorizontalIcon color="#00CCBB" /> 
+      </View>
+      <ScrollView className="bg-gray-100 "
+       contentContainerStyle={{
+        paddingBottom:100,
+      }}>
+        {/* Categories */}
+          <Categories />
+
+          {/* Featured Rows */}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
